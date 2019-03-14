c06af3f289c8d17ec4c13d3492ac29f87625995e
Gregory Ho <Gregory_Ho@ultimatesoftware.com>
2019-03-13 23:14:08 -0400

    Moved nav bar animation into event listener function for window resize responsiveness

--- frontend/WorkBoard/src/app/app.module.ts
+++ frontend/WorkBoard/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HomeComponent } from "./home/home.component";
+
+import {$} from "jquery";
 
 @NgModule({
   declarations: [
--- frontend/WorkBoard/src/app/home/home.component.ts
+++ frontend/WorkBoard/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
 
@@ -19,9 +21,28 @@ export class HomeComponent implements OnInit {
   private analyticsTitle: string = "Analytics";
   private settingsTitle: string = "Settings";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onMouseEnter(): void {
+    $(".container-fluid").stop().dequeue().delay(350).animate({
+      width: "15vw"
+    }, {
+      duration: "200",
+      easing: "swing"
+    });
+  }
+
+  onMouseLeave(): void {
+    $(".container-fluid").stop().dequeue().delay(200).animate({
+      width: "5vw"
+    }, {
+      duration: "200",
+      easing: "swing"
+    });
   }
 
 }
