40d8c68f3a8d4e93d1837050e811e873fff32abb
Gregory Ho <Gregory_Ho@ultimatesoftware.com>
2019-03-15 00:22:26 -0400

    Added top nav bar and some media queries

--- frontend/WorkBoard/src/app/home/home.component.ts
+++ frontend/WorkBoard/src/app/home/home.component.ts
@@ -1,25 +1,25 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 
-import * as $ from "jquery";
+import * as $ from 'jquery';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  private homeIcon: string = "assets/icons/logo_icon.png";
-  private tasksIcon: string = "assets/icons/tasks_icon.png";
-  private plannerIcon: string = "assets/icons/planner_icon.png";
-  private analyticsIcon: string = "assets/icons/analytics_icon.png";
-  private settingsIcon: string = "assets/icons/settings_icon.png";
+  homeIcon: string = 'assets/icons/logo_icon.png';
+  tasksIcon: string = 'assets/icons/tasks_icon.png';
+  plannerIcon: string = 'assets/icons/planner_icon.png';
+  analyticsIcon: string = 'assets/icons/analytics_icon.png';
+  settingsIcon: string = 'assets/icons/settings_icon.png';
 
-  private homeTitle: string = "Home";
-  private tasksTitle: string = "My Tasks";
-  private plannerTitle: string = "Planner";
-  private analyticsTitle: string = "Analytics";
-  private settingsTitle: string = "Settings";
+  homeTitle: string = 'Home';
+  tasksTitle: string = 'My Tasks';
+  plannerTitle: string = 'Planner';
+  analyticsTitle: string = 'Analytics';
+  settingsTitle: string = 'Settings';
 
   constructor() {
   }
@@ -28,20 +28,20 @@ export class HomeComponent implements OnInit {
   }
 
   onMouseEnter(): void {
-    $(".container-fluid").stop().dequeue().delay(350).animate({
-      width: "15vw"
+    $('.container-fluid').stop().dequeue().delay(350).animate({
+      width: '15vw'
     }, {
-      duration: "200",
-      easing: "swing"
+      duration: '200',
+      easing: 'swing'
     });
   }
 
   onMouseLeave(): void {
-    $(".container-fluid").stop().dequeue().delay(200).animate({
-      width: "5vw"
+    $('.container-fluid').stop().dequeue().delay(350).animate({
+      width: '5vw'
     }, {
-      duration: "200",
-      easing: "swing"
+      duration: '200',
+      easing: 'swing'
     });
   }
 
