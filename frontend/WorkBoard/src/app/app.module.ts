5350bad2aca1df43e758b1ef9be3e75072612b04
Gregory Ho <Gregory_Ho@ultimatesoftware.com>
2019-04-06 22:04:43 -0400

    Added flex properties for tags. Added groundwork for detail pop out.

--- frontend/WorkBoard/src/app/app.module.ts
+++ frontend/WorkBoard/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import { ConvertToDueInPipe } from './work/convert-to-due-in.pipe';
 import { CreateModalComponent } from './work/create-modal/create-modal.component';
+import { TaskDetailComponent } from './work/task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CreateModalComponent } from './work/create-modal/create-modal.component
     ToolbarComponent,
     WorkComponent,
     ConvertToDueInPipe,
-    CreateModalComponent
+    CreateModalComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
