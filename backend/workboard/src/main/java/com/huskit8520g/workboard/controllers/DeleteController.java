72e40a16b43ff3d6fbee6b6d4b40a6321fca6a62
Gregory Ho <Gregory_Ho@ultimatesoftware.com>
2019-04-07 23:01:36 -0400

    Added Delete endpoint.

--- backend/workboard/src/main/java/com/huskit8520g/workboard/controllers/DeleteController.java
+++ backend/workboard/src/main/java/com/huskit8520g/workboard/controllers/DeleteController.java
@@ -1,26 +1,23 @@
 package com.huskit8520g.workboard.controllers;
 
-import com.huskit8520g.workboard.models.Task;
 import com.huskit8520g.workboard.repositories.TaskRepository;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api", method = RequestMethod.POST)
+@RequestMapping(path = "/api", method = RequestMethod.DELETE)
 @CrossOrigin(origins = "http://localhost:4200")
-public class PostController {
+public class DeleteController {
 
   @Autowired
   TaskRepository repository;
 
-  @RequestMapping("/tasks")
-  public Task createTask(@Valid @RequestBody Task body) {
-    return repository.save(body);
+  @RequestMapping("/tasks/{id}")
+  public void deleteTask(@PathVariable int id) {
+    repository.deleteById(id);
   }
-
 }
--- backend/workboard/src/main/java/com/huskit8520g/workboard/controllers/GetController.java
+++ backend/workboard/src/main/java/com/huskit8520g/workboard/controllers/GetController.java
@@ -4,9 +4,9 @@ import com.huskit8520g.workboard.models.Task;
 import com.huskit8520g.workboard.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +18,12 @@ public class GetController {
   TaskRepository repository;
 
   @RequestMapping(path = "/tasks")
-  public Iterable<Task> getAllTasks(){
+  public Iterable<Task> getAllTasks() {
     return repository.findAll();
   }
 
   @RequestMapping(path = "/tasks/{id}")
-  public Task getTaskById(@RequestParam int id){
+  public Task getTaskById(@PathVariable int id) {
     return repository.findById(id).get();
   }
 }
